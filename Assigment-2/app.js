face4863dbd7e754339c013647d449a1c9f0cc31
Muhammad Yasir <yasirtahir009@gmail.com>
2024-03-05 11:01:32 +0500

    Initial commit

---
+++ Assigment-2/app.js
@@ -0,0 +1,80 @@
+// Chapter#4
+
+// Q1
+//  var name = "My name is Muhammad Yasir.";
+//  alert(name);
+
+//  var age  = "My age is 23 years old. ";
+//  alert(age);
+
+//  var year = "My Birth year is 2000.";
+//  alert(year);
+
+// Q2
+// Legal Variables Names:
+
+// var user = "Muhammad Yasir";
+// alert(user);
+
+// var heading = "Asalam-O-Alaikum";
+// alert(heading);
+
+// var fullName = "Muhammd Yasir Tahir";
+// alert(fullName);
+
+// var full_name = "Muhammad Yasir Tahir";
+// alert(full_name);
+ 
+// var userdocument = "index.html";
+// alert(userdocument);
+
+// Q2
+// Illegal Variable Names:
+
+// var abc = "Asalam-O-Alaikum.";
+// alert(abc);
+
+// var number = "18";
+// alert(number);
+
+// var document = "app.js";
+// alert(document);
+
+// var fullnamee = "Muhammad Yasir";
+// alert(fullnamee);
+
+// var
+
+
+
+
+// Q3
+
+
+// var age;
+// age = 10;
+
+// console.log("Value after declaration is "+age)
+
+
+// var number = 18;
+
+// console.log("Initial value "+number)
+
+// number++
+
+// console.log(number);
+
+// // number += 7;
+
+// number = number + 7
+
+// console.log(number);
+
+// number--
+
+// console.log(number);
+
+// number = number % 3
+
+// console.log(number);
